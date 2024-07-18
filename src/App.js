18c1aac485a5b913cbffc92758a38caa13ab2737
Sweta biswas <swetabiswas452@gmail.com>
2024-07-19 01:04:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import 'react-notifications/lib/notifications.css';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
 function App() {
+  let [todolist, setTodolist] = useState([]);
+
+  let saveToDoList = (event) => {
+    event.preventDefault();
+    let toname = event.target.toname.value;
+
+    if (!todolist.some(item => item.name === toname)) {
+      let finaldolist = [...todolist, { name: toname, completed: false }];
+      setTodolist(finaldolist);
+    } else {
+      showNotification();
+    }
+  };
+
+  let showNotification = () => {
+    NotificationManager.warning('ToDo name already exists..');
+  };
+
+  let toggleStatus = (index) => {
+    let updatedList = [...todolist];
+    updatedList[index].completed = !updatedList[index].completed;
+    setTodolist(updatedList);
+  };
+
+  let deleteRow = (index) => {
+    let updatedList = todolist.filter((_, i) => i !== index);
+    setTodolist(updatedList);
+  };
+
+  let list = todolist.map((item, index) => {
+    return (
+      <ToDoListItems
+        key={index}
+        indexNumber={index}
+        item={item}
+        toggleStatus={toggleStatus}
+        deleteRow={deleteRow}
+      />
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List App</h1>
+      <form onSubmit={saveToDoList}>
+        <input type="text" name="toname" />
+        <button type="submit">Save</button>
+        <NotificationContainer />
+      </form>
+
+      <div className="outerDiv">
+        <ul>
+          {list}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({ item, indexNumber, toggleStatus, deleteRow }) {
+  return (
+    <li className={item.completed ? 'completetodo' : ''} onClick={() => toggleStatus(indexNumber)}>
+      {indexNumber + 1} {item.name} <span onClick={(e) => { e.stopPropagation(); deleteRow(indexNumber); }}>&times;</span>
+    </li>
+  );
+}
